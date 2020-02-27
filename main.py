b607a3df7015e8c497d412f65af2143ec3839b4d
Demokdawa <Demokdawa@yahoo.fr>
2020-02-27 17:39:51 +0100

    update

--- main.py
+++ main.py
@@ -18,6 +18,8 @@ import sqlite3
 import unidecode
 from random import randint
 from pathlib import Path
+from colour import Color
+from spellchecker import SpellChecker
 
 #INIT&VARS####################################################################################################
 
@@ -43,20 +45,38 @@ pos_list = [(483, 411, 709, 458), (725, 411, 951, 458), (968, 411, 1193, 458), (
 #UI-COLORS####################################################################################################
 
 # RGB Format
-ui_color_list = [
-    (189, 168, 101, 'Virtuvian'),   # Vitruvian
-    (150, 31, 35, 'Stalker'),       # Stalker 
-    (238, 193, 105, 'Baruk'),       # Baruk
-    (35, 200, 245, 'Corpus'),       # Corpus
-    (57, 105, 192, 'Fortuna'),      # Fortuna
-    (255, 189, 102, 'Grineer'),     # Grineer
-    (36, 184, 242, 'Lotus'),        # Lotus
-    (140, 38, 92, 'Nidus'),         # Nidus
-    (20, 41, 29, 'Orokin'),         # Orokin
-    (9, 78, 106, 'Tenno'),          # Tenno
-    (2, 127, 217, 'High contrast'), # High contrast
-    (255, 255, 255, 'Legacy'),      # Legacy
-    (158, 159, 167, 'Equinox')      # Equinox
+ui_color_list_primary = [
+    (190, 169, 102, 'Virtuvian'),             # Vitruvian
+    (153,  31,  35, 'Stalker'),               # Stalker 
+    (238, 193, 105, 'Baruk'),                 # Baruk
+    ( 35, 201, 245, 'Corpus'),                # Corpus
+    ( 57, 105, 192, 'Fortuna'),               # Fortuna
+    (255, 189, 102, 'Grineer'),               # Grineer
+    ( 36, 184, 242, 'Lotus'),                 # Lotus
+    (140,  38,  92, 'Nidus'),                 # Nidus
+    ( 20,  41,  29, 'Orokin'),                # Orokin
+    (  9,  78, 106, 'Tenno'),                 # Tenno
+    (  2, 127, 217, 'High contrast'),         # High contrast
+    (255, 255, 255, 'Legacy'),                # Legacy
+    (158, 159, 167, 'Equinox'),               # Equinox
+    (140, 119, 147, 'Dark Lotus')             # Dark Lotus
+]
+
+ui_color_list_secondary = [
+    (245, 227, 173, 'Virtuvian'),   
+    (255,  61,  51, 'Stalker'),     
+    (236, 211, 162, 'Baruk'),       
+    (111, 229, 253, 'Corpus'),      
+    (255, 115, 230, 'Fortuna'),     
+    (255, 224, 153, 'Grineer'),     
+    (255, 241, 191, 'Lotus'),           
+    (245,  73,  93, 'Nidus'),           
+    (178, 125,   5, 'Orokin'),  
+    (  6, 106,  74, 'Tenno'),           
+    (255, 255,   0, 'High contrast'),   
+    (232, 213,  93, 'Legacy'),      
+    (232, 227, 227, 'Equinox'), 
+    (200, 169, 237, 'Dark Lotus')
 ]
 
 # Check ui theme from screenshot (Image Format : OPENCV)
@@ -69,31 +89,126 @@ def get_theme(image, color_treshold):
             pass
     
 ##############################################################################################################
-#####TEST#####################################################################################################
+#####TRESHOLD#################################################################################################
 
-# Check ui theme from screenshot
-def check_pix(input_pix_clr, input_theme, color_treshold):
-    e = (189, 168, 101, 'Virtuvian')
-    if abs(input_pix_clr[2] - e[0]) < color_treshold and abs(input_pix_clr[1] - e[1]) < color_treshold and abs(input_pix_clr[0] - e[2]) < color_treshold:
-        return True
-    else:
-        return False
-            
-def tresh(image):
-    h = image.shape[0] # Hauteur
-    w = image.shape[1] # Largeur
-    for y in range(0, h):
-        for x in range(0, w):
-            pix_crl = image[y, x]
-            if check_pix(pix_crl, 'Stalker', 30) is True:
-                image[y, x] = 0
-            else:
-                image[y, x] = 255
+
+# Treshold Function
+def get_treshold_2(image, theme):
+
+    e_primary = [item for item in ui_color_list_primary if item[3] == theme][0]
+    e_secondary = [item for item in ui_color_list_secondary if item[3] == theme][0]
+
+    c_primary = Color(rgb=(e_primary[0] / 256, e_primary[1] / 256, e_primary[2] / 256))
+    c_secondary = Color(rgb=(e_secondary[0] / 256, e_secondary[1] / 256, e_secondary[2] / 256))
+
+    upscaled = cv2.resize(image, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)  # Upscaling x2
     
-    return image
+    rgb_arr = image
+    hsl_arr = cv2.cvtColor(upscaled, cv2.COLOR_BGR2HLS)  # Hue, Lighness, Saturation
+    
+    p_hue = round(c_primary.hue * 360) / 2
+    p_hue_sec = round(c_secondary.hue * 360) / 2
+    
+    kernel = np.ones((3, 3), np.uint8)
+
+    if theme == 'Virtuvian':  # WORKING
+        method = 'HSL'
+        HueOK = np.logical_and(hsl_arr[..., 0] > p_hue - 4 / 2, hsl_arr[..., 0] < p_hue + 4 / 2)
+        SaturationOK = hsl_arr[..., 2] >= (0.25 * 256)
+        LightnessOK = hsl_arr[..., 1] >= (0.42 * 256)
+        combinedMask = HueOK & SaturationOK & LightnessOK
+        
+    elif theme == 'Stalker':  # WORKING
+        method = 'HSL'
+        HueOK = np.logical_and(hsl_arr[..., 0] > p_hue - 4 / 2, hsl_arr[..., 0] < p_hue + 4 / 2)
+        SaturationOK = hsl_arr[..., 2] >= (0.5 * 256)
+        LightnessOK = hsl_arr[..., 1] >= (0.20 * 256)
+        combinedMask = HueOK & SaturationOK & LightnessOK
+        
+    elif theme == 'Baruk':
+        pass
+        
+    elif theme == 'Corpus':
+        pass
+        
+    elif theme == 'Fortuna':  # WORKING
+        method = 'HSL'
+        HueOK = np.logical_and(hsl_arr[..., 0] > p_hue - 4 / 2, hsl_arr[..., 0] < p_hue + 4 / 2)
+        SaturationOK = hsl_arr[..., 2] >= (0.20 * 256)
+        LightnessOK = hsl_arr[..., 1] >= (0.25 * 256)
+        combinedMask = HueOK & SaturationOK & LightnessOK
+        
+    elif theme == 'Grineer':
+        pass
+        
+    elif theme == 'Lotus':  # A TESTER
+        method = 'HSL'
+        HueOK = np.logical_and(hsl_arr[..., 0] > p_hue - 5 / 2, hsl_arr[..., 0] < p_hue + 5 / 2)
+        SaturationOK = hsl_arr[..., 2] >= (0.65 * 256)
+        LightnessOK = np.logical_and(hsl_arr[..., 1] >= p_lumi - 0.1, hsl_arr[..., 0] <= p_lumi + 0.1)
+        # return Math.Abs(test.GetHue() - primary.GetHue()) < 5 & test.GetSaturation() >= 0.65 & & Math.Abs(
+        #    test.GetBrightness() - primary.GetBrightness()) <= 0.1
+        # | | (Math.Abs(test.GetHue() - secondary.GetHue()) < 4 & & test.GetBrightness() >= 0.65);
+        combinedMask = HueOK & SaturationOK & LightnessOK
+        
+    elif theme == 'Nidus':
+        method = 'BGR'
+        lowerBound = np.array([(e_primary[2] - 40), (e_primary[1] - 40), (e_primary[0] - 40)])  # BGR
+        upperBound = np.array([(e_primary[2] + 40), (e_primary[1] + 40), (e_primary[0] + 40)])  # BGR
+
+    elif theme == 'Orokin':
+        pass
+        
+    elif theme == 'Tenno':
+        pass
+        
+    elif theme == 'High contrast':
+        pass
+        
+    elif theme == 'Legacy':  # WORKS but NEED TESTING
+        method = 'HSL'
+        SaturationOK = hsl_arr[..., 2] <= (0.2 * 256)
+        LightnessOK = hsl_arr[..., 1] >= (0.75 * 256)
+        combinedMask = SaturationOK & LightnessOK
+        pass
+        
+    elif theme == 'Equinox':  # WORKING
+        method = 'HSL'
+        HueOK = np.logical_and(hsl_arr[..., 0] > 110, hsl_arr[..., 0] < 135)
+        SaturationOK = hsl_arr[..., 2] <= (0.1 * 255)
+        LightnessOK = np.logical_and(hsl_arr[..., 1] >= (0.35 * 256), hsl_arr[..., 1] <= (0.74 * 256))
+        combinedMask = HueOK & SaturationOK & LightnessOK
+
+    elif theme == 'Dark Lotus':  # WORKING
+        method = 'HSL'
+        HueOK = np.logical_and(hsl_arr[..., 0] > 134, hsl_arr[..., 0] < 143)
+        SaturationOK = np.logical_and(hsl_arr[..., 2] >= (0.11 * 256), hsl_arr[..., 2] <= (0.22 * 256))
+        LightnessOK = np.logical_and(hsl_arr[..., 1] >= (0.43 * 256), hsl_arr[..., 2] <= (0.53 * 256))
+        combinedMask = HueOK & SaturationOK & LightnessOK
+    
+    if method == 'BGR':
+        filter = cv2.inRange(upscaled, lowerBound, upperBound)
+        filered = cv2.bitwise_not(filter)
+        tresh = cv2.erode(filered, kernel, iterations=1)
+        
+    elif method == 'HSL':
+        hsl_arr[combinedMask] = 0
+        hsl_arr[~combinedMask] = 255
+        tresh = cv2.erode(hsl_arr, kernel, iterations=1)
+
+    return tresh
 
 ##############################################################################################################
 
+
+
+# Try to correct mistakes
+def spell_correction_ocr(string, corr_list):
+    spell_check_ocr = SpellChecker(distance=2, language=None, case_sensitive=False)
+    spell_check_ocr.word_frequency.load_words(corr_list)
+    spell_check_ocr.correction(string)
+    return spell_check_ocr.correction(string).strip().capitalize()
+    
 
 def parse_language():
     if language == 'FR':
@@ -104,37 +219,6 @@ def parse_language():
         return ref_language
     else:
         pass
-
-# Image processing for better detection after
-def create_mask(theme, img):
-    if theme == 'Virtuvian':
-        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        #lower_virtu = np.array([-3, 80, 80])
-        #upper_virtu = np.array([43, 255, 255])
-        lower_virtu = np.array([20, 90, 110])
-        upper_virtu = np.array([43, 135, 255])
-        mask = cv2.inRange(hsv, lower_virtu, upper_virtu)
-        return mask
-    if theme == 'Stalker':
-        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        lower_stalk = np.array([159, 80, 80])
-        upper_stalk = np.array([199, 255, 255])
-        mask = cv2.inRange(hsv, lower_stalk, upper_stalk)
-        return mask
-    if theme == 'Ancient':
-        return img
-    if theme == 'Equinox':
-        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        lower_equi = np.array([107, 0, 0])
-        upper_equi = np.array([127, 255, 255])
-        mask = cv2.inRange(hsv, lower_equi, upper_equi)
-        return mask
-    if theme == 'Fortuna':
-        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        lower_equi = np.array([80, 80, 84])
-        upper_equi = np.array([120, 199, 255])
-        mask = cv2.inRange(hsv, lower_equi, upper_equi)
-        return mask
         
 
 # Crop an area of a relic
@@ -164,17 +248,13 @@ def normalize_names(name):
         return translation_format
 
 def data_pass_name(pos1, pos2, pos3, pos4, image2):
-    img_file = str(folder) + '/my_screenshot.png'
+    img_file = str(folder) + '/test_ducat.png'
     image = cv2.imread(img_file)
     cropped_img = relicarea_crop(pos1, pos2, pos3, pos4, image)
-    upscaled = cv2.resize(cropped_img, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
-    # ret, imgtresh = cv2.threshold(create_mask('Virtuvian', upscaled), 218, 255, cv2.THRESH_BINARY_INV)
-    imgtresh_temp = tresh(upscaled)
-    kernel = np.ones((1, 1), np.uint8)
-    imgtresh = cv2.erode(imgtresh_temp, kernel, iterations=1)
-    
-    tessdata_dir_config = '--tessdata-dir "' + tessdata_path + '" -l Roboto --oem 1 --psm 6'
-    textocr = pytesseract.image_to_string(imgtresh, config=tessdata_dir_config)
+    imgtresh_temp = get_treshold_2(cropped_img, 'Virtuvian')
+    tessdata_dir_config = '--tessdata-dir "' + tessdata_path + '" -l Roboto --oem 1 --psm 6 get.images'
+    textocr = pytesseract.image_to_string(imgtresh_temp, config=tessdata_dir_config)
+    corrected = spell_correction_ocr(textocr, corr_list)
     print('ocr is : ' + textocr)
     return textocr
     
